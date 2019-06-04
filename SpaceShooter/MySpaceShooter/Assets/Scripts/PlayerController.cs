b5ecdc9001760a0579034ed86a93630aa792d9db
varos60 <alvaro.p.c.162@gmail.com>
2019-06-05 00:50:18 +0200

    Subida de Proyectos nuevos y finalizacion de SpaceShotter

--- SpaceShooter/MySpaceShooter/Assets/Scripts/PlayerController.cs
+++ SpaceShooter/MySpaceShooter/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public class Boundary
 {
-    public float xMin, xMax;
+    public float xMin, xMax, zMin, zMax;
 }
 
 public class PlayerController : MonoBehaviour{
@@ -14,9 +14,37 @@ public class PlayerController : MonoBehaviour{
     public float tilt;
     public Boundary boundary;
 
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire = 0.0f;
+
+    private AudioSource audioSource;
+
+    protected Joystick joystick;
+    protected Joystick2 joybutton;
+
+    public GameObject gamepad;
+
     void Start()
     {
          rb = GetComponent<Rigidbody>();
+         audioSource = GetComponent<AudioSource>();
+         if (Application.platform == RuntimePlatform.Android)
+         {
+            joystick = FindObjectOfType<Joystick>();
+            joybutton = FindObjectOfType<Joystick2>();
+         }
+         else
+         {
+            gamepad.SetActive(false);
+         }
+    }
+
+    void Update()
+    {
+        Shot();
+        
     }
 
     void FixedUpdate()
@@ -24,7 +52,7 @@ public class PlayerController : MonoBehaviour{
         Movement();
     }
 
-    void Movement() {
+    /*void Horizontal_Movement() {
         float horizontal_axis = Input.GetAxis("Horizontal");
         // rb.AddForce(new Vector3(speed*horizontal_axis, 0, 0));
         rb.velocity = new Vector3 (speed*horizontal_axis, 0.0f, 0.0f);
@@ -34,5 +62,50 @@ public class PlayerController : MonoBehaviour{
             0.0f
             );
         rb.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rb.velocity.x*-tilt));
+    }*/
+
+    void Movement()
+    {
+        float horizontal_axis ;
+        float vertical_axis;
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            horizontal_axis = joystick.Horizontal;
+            vertical_axis = joystick.Vertical;
+        }else
+        {
+            horizontal_axis = Input.GetAxis("Horizontal");
+            vertical_axis = Input.GetAxis("Vertical");
+        }
+       // rb.AddForce(new Vector3(speed*horizontal_axis, 0, 0));
+       rb.velocity = new Vector3(speed * horizontal_axis, 0.0f, speed * vertical_axis);
+       rb.position = new Vector3(
+           Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+           0.0f,
+           Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+           );
+       rb.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rb.velocity.x * -tilt));       
+    }
+
+
+   void Shot()
+   {
+       if (Application.platform == RuntimePlatform.Android)
+       {
+        if (joybutton.fire && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject clone = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+                audioSource.Play();
+            }
+        }else
+        {
+            if (Input.GetButton("Fire1") && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject clone = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+                audioSource.Play();
+            }
+        }
     }
 }
