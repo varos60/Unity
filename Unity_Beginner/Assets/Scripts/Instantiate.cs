c7cff43bef7c341e1ed1fac0a0dd8ba205e9d51a
varos60 <alvaro.p.c.162@gmail.com>
2019-09-25 19:04:30 +0200

    Actualizacion Procedimientos

---
+++ Unity_Beginner/Assets/Scripts/Instantiate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void OnMouseDown()
+    {
+        //Rigidbody rb =  Instantiate<Rigidbody>(prefab.GetComponent<Rigidbody>(), new Vector3(0f, 1.5f, 0f), transform.rotation);        
+        GameObject go = Instantiate(prefab, new Vector3(0f, 1.5f, 0f), transform.rotation);
+        Rigidbody rb = go.GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * 500f);
+    }
+}
---
+++ Unity_Beginner/Assets/Scripts/Mouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse Click");
+        rb.AddForce(Vector3.up * 500f);
+    }
+}
