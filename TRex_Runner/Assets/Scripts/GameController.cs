ad27606193edb78e83ec52525fa29d4b399d24f8
varos60 <alvaro.p.c.162@gmail.com>
2020-01-10 00:43:07 +0100

    TRex_Runner

---
+++ TRex_Runner/Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static bool run;
+    public static bool die;
+
+    void Awake()
+    {
+        run = false;
+        die = false;
+    }
+
+}
---
+++ TRex_Runner/Assets/Scripts/Player.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool ground = true;
+    bool down = false;
+    Animator animator;
+    Rigidbody2D rb;
+    public float jumpForce = 375f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Begin();
+        Jump();
+        FlowJump();
+        Down(); 
+    }
+
+    void Begin()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GameController.run == false && GameController.die == false)
+            {
+                GameController.run = true;
+                animator.SetBool("Run", GameController.run);
+            }
+        }            
+    }
+
+    void Down()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (GameController.run == true && GameController.die == false && ground == true && down == false)
+            {
+                down = true;
+                animator.SetBool("Down", down);
+            }
+        }
+        else if (!Input.GetKey(KeyCode.DownArrow))
+        {
+            if (GameController.run == true && GameController.die == false && ground == true && down == true)
+            {
+                down = false;
+                animator.SetBool("Down", down);
+            }
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (GameController.run == true && GameController.die == false && ground == true)
+            {                
+                ground = false;                               
+                rb.velocity = Vector2.up * jumpForce * Time.deltaTime;
+                animator.SetBool("Ground", ground);
+            }
+        }
+    }
+
+    void FlowJump()
+    {
+        if (rb.velocity.y < 0)
+        {            
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {            
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Enemy")
+        {
+            ground = true;
+            animator.SetBool("Ground", ground);
+        }
+    }
+}
