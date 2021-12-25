ec7b51594c400a26e96c62ffd0de0209bd028f09
Varos60 <alvaro.p.c.162@gmail.com>
2021-12-25 15:01:57 +0100

    Actualizacion Acuario

---
+++ Acuario/Acuario/Assets/Scripts/MenuScripts/MenuMoveBackgound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMoveBackgound : MonoBehaviour
+{    
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBackground();
+    }
+
+    void MoveBackground()
+    {
+        transform.Translate(Vector2.left * speed * 0.0001f);
+        if(transform.position.x <= -4.334f)
+        {            
+            ResetBackgroundPosition();
+        }
+    }
+
+    void ResetBackgroundPosition()
+    {
+        transform.position = new Vector2(0.78f, 0);
+    }
+}
